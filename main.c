3302965165a0ac424993f2e294bc68c95056ca4f
luispaiva40200280 <luispaiva8936@gmail.com>
2025-11-19 02:35:07 +0000

    rm test.c

--- main.c
+++ main.c
@@ -1,51 +1,95 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lpaiva <lpaiva@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/17 19:51:33 by lpaiva            #+#    #+#             */
-/*   Updated: 2025/11/17 20:01:46 by lpaiva           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int     fd;
-    char    *line;
-    int     line_count = 0;
-    
-    if (argc > 2)
-        return (printf("Usage: %s <file>\n", argv[0]), 1);
-    fd = open(argv[1], O_RDONLY);
-    if (fd < 0)
-        return (printf("Error opening file\n"), 1);
-    
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("Line %d: %s", ++line_count, line);
-        free(line);  // CRITICAL: Free each line
-    }
-    
-    close(fd);
-    return (0);
-}  
-/* 
-if (ft_strchr(result, '\n'))  // Stop when newline found
-static char	*buffer[1024]; //result of reading the file
-char		*line_read; // line that is goind to be return
-//error handler :: if fd does not exist or is empty;
-if (fd < 0 || fd >= 1024 || BUFFER_SIZE <= 0)
-char	*buff; // store the value read of the file 
-int		bytes; //bytes read 
+	int		*fds;
+	char	*line;
+	int		i;
+	int		round;
+	int		active_fds;
 
+	if (argc < 2)
+	{
+		printf("Usage: %s <file1> <file2> <file3> ...\n", argv[0]);
+		printf("Example: %s file1.txt file2.txt file3.txt\n", argv[0]);
+		return (1);
+	}
 
-	if (!buff[i]) // end of the file;
-	int i; // for s
-    	// handle to error
-	if (!buffer[fd])
-		return (NULL);
-*/
+	// Alocar array para file descriptors
+	fds = malloc(sizeof(int) * (argc - 1));
+	if (!fds)
+		return (printf("Error: malloc failed\n"), 1);
+
+	// Abrir todos os arquivos
+	printf("Opening %d files...\n", argc - 1);
+	for (i = 0; i < argc - 1; i++)
+	{
+		fds[i] = open(argv[i + 1], O_RDONLY);
+		if (fds[i] < 0)
+		{
+			printf("Error: Could not open '%s'\n", argv[i + 1]);
+			// Fechar os já abertos
+			while (--i >= 0)
+				close(fds[i]);
+			free(fds);
+			return (1);
+		}
+		printf("  FD%d: %s (fd=%d)\n", i, argv[i + 1], fds[i]);
+	}
+
+	printf("\n=== Starting Multiple FD Test ===\n\n");
+
+	round = 1;
+	active_fds = argc - 1;
+	
+	while (active_fds > 0)
+	{
+		printf("--- Round %d ---\n", round);
+		active_fds = 0;
+
+		// Ler de cada file descriptor
+		for (i = 0; i < argc - 1; i++)
+		{
+			if (fds[i] >= 0)  // Se ainda está aberto
+			{
+				line = get_next_line(fds[i]);
+				if (line)
+				{
+					printf("FD%d (%s): %s", i, argv[i + 1], line);
+					free(line);
+					active_fds++;
+				}
+				else
+				{
+					printf("FD%d (%s): (null) - EOF reached\n", i, argv[i + 1]);
+					close(fds[i]);
+					fds[i] = -1;  // Marcar como fechado
+				}
+			}
+		}
+		printf("\n");
+		round++;
+
+		// Segurança: parar após 100 rounds
+		if (round > 100)
+		{
+			printf("Warning: Stopped after 100 rounds\n");
+			break ;
+		}
+	}
+
+	printf("=== All files read successfully ===\n");
+	printf("Total rounds: %d\n", round - 1);
+
+	// Fechar qualquer FD ainda aberto
+	for (i = 0; i < argc - 1; i++)
+	{
+		if (fds[i] >= 0)
+			close(fds[i]);
+	}
+	free(fds);
+
+	return (0);
+}
